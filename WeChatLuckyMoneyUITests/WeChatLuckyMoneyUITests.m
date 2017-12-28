dd74d72ce266abd1139cfc8c38c8aeddf8a77239
Larry Tin <dev@goodow.com>
2017-12-28 17:43:21 +0800

    简化代码

--- WeChatLuckyMoneyUITests/WeChatLuckyMoneyUITests.m
+++ WeChatLuckyMoneyUITests/WeChatLuckyMoneyUITests.m
@@ -16,31 +16,19 @@
 @implementation WeChatLuckyMoneyUITests
 
 - (void)setUp {
-  UIApplication.sharedApplication.keyWindow.layer.speed = 1000;
-  [UIView setAnimationsEnabled:NO];
   [super setUp];
 
-  // Put setup code here. This method is called before the invocation of each test method in the class.
-
-  // In UI tests it is usually best to stop immediately when a failure occurs.
   self.continueAfterFailure = YES;
-  // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
   _app = [[XCUIApplication alloc] initWithBundleIdentifier:@"com.tencent.xin"];
   [_app launch];
 
-  // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
 - (void)tearDown {
-  // Put teardown code here. This method is called after the invocation of each test method in the class.
   [super tearDown];
 }
 
 - (void)testExample {
-  XCUIElement *xcuiElement = _app.tables.firstMatch.staticTexts.firstMatch;
-//  XCUIElement *xcuiElement = [_app.tables.firstMatch.cells elementBoundByIndex:2];
-  [xcuiElement tap];
-
   XCUIElement *element = [self waitUntilRedEnvelopExists];
   [element tap];
 
@@ -49,18 +37,12 @@
     [button tap];
   }
   
-  NSLog(@"%@", _app.debugDescription);
 }
 
 - (XCUIElement *)waitUntilRedEnvelopExists {
   XCUIElement *element = [self findRedEnvelop];
   while (!element.exists) {
-//    if ([NSDate timeIntervalSinceReferenceDate] - startTime > timeout) {
-//      XCTFail(@"Timed out waiting for element to exist");
-//      return;
-//    }
     CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.1, false);
-    element = [self findRedEnvelop];
   }
   return element;
 }
